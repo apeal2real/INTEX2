0f7e19895e3afda10c514f52d346e7f87ec3a44e
apeal2real <alec.peal@gmail.com>
2024-04-03 10:45:01 -0600

    Initial Setup
    DONT FORGET TO UNCOMMENT THE MODELS LINES IN  THE HOMECONTROLLER AND VIEWIMPORTS

--- INTEX2/Controllers/HomeController.cs
+++ INTEX2/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-using INTEX2.Models;
+//using INTEX2.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,27 +6,9 @@ namespace INTEX2.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
